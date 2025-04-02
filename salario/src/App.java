d8a378e847e6772a6cfcbea6a087f64c45049b34
Leofront-end <ls171258@gmail.com>
2025-04-02 18:18:49 -0300

    salario

---
+++ salario/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int num, horas;
+        float valor, salario;
+
+        num = sc.nextInt();
+        horas = sc.nextInt();
+        valor = sc.nextFloat();
+        salario = valor * horas;
+
+        System.out.println("NUMBER = " + num);
+        System.out.printf("SALARY = U$ %.2f",salario);
+
+    }
+}
