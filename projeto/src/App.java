adba2d22ff18f79173efa876053f2572a0cdfe16
Leofront-end <ls171258@gmail.com>
2025-04-02 17:50:15 -0300

    produto simples

---
+++ projeto/src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a,b,prod;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        prod = a * b;
+
+        System.out.println("PROD = " + prod);
+    }
+}
